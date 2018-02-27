b663ea19f7e67feae700a56a9e82e0d676df1582
George Dwyer <george.dwyer@ucl.ac.uk>
2018-02-27 19:21:10 +0000

    Adding adjustment to the slam node to mark open cells in range of the lasers as open

--- comp313p_planner_controller/scripts/mock_slam_node.py
+++ comp313p_planner_controller/scripts/mock_slam_node.py
@@ -13,70 +13,93 @@ from comp313p_planner_controller.cell import CellLabel
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 
-#This class creates a simulation of SLAM data:
+
+# This class creates a simulation of SLAM data:
 # uses True data to create a map that is initialised from robot starting location and publishes robot odometry
 #  that is initialised at starting location
 
 class Mock_Slam_Node(object):
-
     def __init__(self):
         rospy.init_node('mock_slam_node', anonymous=True)
-	rospy.wait_for_service('static_map')
-	rospy.loginfo('------> 0')
+        rospy.wait_for_service('static_map')
+        rospy.loginfo('------> 0')
         self.mapServer = rospy.ServiceProxy('static_map', GetMap)
-	rospy.loginfo('------> 1')
+        rospy.loginfo('------> 1')
         resp = self.mapServer()
         self.occupancyGrid = OccupancyGrid(resp.map.info.width, resp.map.info.height, resp.map.info.resolution)
-	self.occupancyGrid.setScale(rospy.get_param('plan_scale',5))
-	self.occupancyGrid.scaleMap()
-	rospy.loginfo('------> 2')
+        self.occupancyGrid.setScale(rospy.get_param('plan_scale', 5))
+        self.occupancyGrid.scaleMap()
+        rospy.loginfo('------> 2')
         self.searchGrid = SearchGrid.fromOccupancyGrid(self.occupancyGrid)
-	rospy.loginfo('------> 3')
-        self.gridDrawer = SearchGridDrawer('Map',self.searchGrid,600)
-	self.gridDrawer.open()
-	rospy.loginfo('------> 4')
+        rospy.loginfo('------> 3')
+        self.gridDrawer = SearchGridDrawer('Map', self.searchGrid, 600)
+        self.gridDrawer.open()
+        rospy.loginfo('------> 4')
         self.local_odometry = Odometry()
-        self.laser_sub= rospy.Subscriber("robot0/laser_0", LaserScan, self.parse_scan)
+        self.laser_sub = rospy.Subscriber("robot0/laser_0", LaserScan, self.parse_scan)
         self.odom_sub = rospy.Subscriber("robot0/odom", Odometry, self.update_odometry)
-        #rospy.Timer(rospy.Duration(1),self.update_visualisation)
-	rospy.loginfo('------> Initialised')
+        # rospy.Timer(rospy.Duration(1),self.update_visualisation)
+        rospy.loginfo('------> Initialised')
 
-    def update_odometry(self,msg):
-        self.local_odometry=msg
+    def update_odometry(self, msg):
+        self.local_odometry = msg
 
-    def parse_scan(self,msg):
-	
-        for ii in range(int(math.floor((msg.angle_max-msg.angle_min)/msg.angle_increment))):
-	    #rospy.loginfo("{} {} {}".format(msg.ranges[ii],msg.angle_min,msg.angle_max))
-	    valid=(msg.ranges[ii]>msg.range_min) and (msg.ranges[ii]<msg.range_max)	    
-	    if valid:
-	 	quaternion=(self.local_odometry.pose.pose.orientation.x, self.local_odometry.pose.pose.orientation.y, self.local_odometry.pose.pose.orientation.z, self.local_odometry.pose.pose.orientation.w)
-	    	euler=tf.transformations.euler_from_quaternion(quaternion)
-            	angle=msg.angle_min+msg.angle_increment*ii + euler[2]
-            
-            
-                point_world_coo=[math.cos(angle)*msg.ranges[ii]+self.local_odometry.pose.pose.position.x, math.sin(angle)*msg.ranges[ii]+self.local_odometry.pose.pose.position.y ]
+    def parse_scan(self, msg):
+
+        for ii in range(int(math.floor((msg.angle_max - msg.angle_min) / msg.angle_increment))):
+            # rospy.loginfo("{} {} {}".format(msg.ranges[ii],msg.angle_min,msg.angle_max))
+            valid = (msg.ranges[ii] > msg.range_min) and (msg.ranges[ii] < msg.range_max)
+            if valid:
+                quaternion = (self.local_odometry.pose.pose.orientation.x, self.local_odometry.pose.pose.orientation.y,
+                              self.local_odometry.pose.pose.orientation.z, self.local_odometry.pose.pose.orientation.w)
+                euler = tf.transformations.euler_from_quaternion(quaternion)
+                angle = msg.angle_min + msg.angle_increment * ii + euler[2]
+
+                point_world_coo = [math.cos(angle) * msg.ranges[ii] + self.local_odometry.pose.pose.position.x,
+                                   math.sin(angle) * msg.ranges[ii] + self.local_odometry.pose.pose.position.y]
+
                 point_cell_coo = self.occupancyGrid.getCellCoordinatesFromWorldCoordinates(point_world_coo)
-                self.occupancyGrid.setCell(point_cell_coo[0], point_cell_coo[1],1)
-		#rospy.loginfo("cell {} {} set".format(point_cell_coo[0], point_cell_coo[1]))
-	#self.searchGrid SearchGrid.fromOccupancyGrid(self.occupancyGrid)
-	self.searchGrid.updateFromOccupancyGrid()
-		
+                robot_world_coo = [self.local_odometry.pose.pose.position.x, self.local_odometry.pose.pose.position.y]
+                robot_cell_coo = self.occupancyGrid.getCellCoordinatesFromWorldCoordinates(robot_world_coo)
+                between = self.ray_trace(robot_cell_coo, point_cell_coo)
 
-    def update_visualisation(self):	 
-	#self.gridDrawer.setSearchGrid(self.searchGrid)       
-	self.gridDrawer.update()
-	
+                for point in between:
+                    self.occupancyGrid.setCell(point[0], point[1], 0)
+
+                self.occupancyGrid.setCell(point_cell_coo[0], point_cell_coo[1], 1)
+                self.searchGrid.updateFromOccupancyGrid()
+
+        self.searchGrid.updateFromOccupancyGrid()
+
+    def ray_trace(self, origin, end):
+        """
+        Function to get a list of points between two points
+        :param origin: position of the origin
+        :param end: position of the end
+        :return: list of points in between the origin and end
+        """
+        diff_x = end[0] - origin[0]
+        diff_y = end[1] - origin[1]
+
+        slope = float(diff_y) / float(diff_x)
+        points = []
+        step = int(diff_x / math.fabs(diff_x))
+        for x in range(origin[0], end[0], step):
+            y = int((slope * x) + origin[1])
+            points.append([x,y])
+
+        return points
+
+
+    def update_visualisation(self):
+        # self.gridDrawer.setSearchGrid(self.searchGrid)
+        self.gridDrawer.update()
+
 
 if __name__ == '__main__':
     mock = Mock_Slam_Node()
 
-    rate=rospy.Rate(10)
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
-	mock.update_visualisation()
-	rate.sleep()
-	
-    
-  
-
-  
+        mock.update_visualisation()
+        rate.sleep()
